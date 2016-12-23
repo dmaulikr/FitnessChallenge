d3d311f09862b368ff3d4178896abe6af17ea5d5
Aaron Martinez <aaronmartinez6@hotmail.com>
2016-12-22 23:08:13 -0700

    Remove references to ResearchKit

--- FitnessChallenge/FitnessChallenge/StandingsViewController.swift
+++ FitnessChallenge/FitnessChallenge/StandingsViewController.swift
@@ -7,41 +7,16 @@
 //
 
 import UIKit
-import ResearchKit
 
 class StandingsViewController: UIViewController {
-    
-//    let barGraphChartDataSource = BarGraphDataSource()
-    let discreteGraphChartDataSource = DiscreteGraphDataSource()
-    
-    @IBOutlet weak var graphView: ORKGraphChartView!
-    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor(red: 45/255, green: 50/255, blue: 55/255, alpha: 1)//Background Dark Gray
         
-        // ORKDiscreteGraphChartView
 
-        guard let discreteGraphChartView = graphView as? ORKDiscreteGraphChartView else { return }
-        discreteGraphChartView.dataSource = discreteGraphChartDataSource
-        discreteGraphChartView.tintColor = UIColor(red: 244/255, green: 190/255, blue: 74/255, alpha: 1)
-        // Optional custom configuration
-        discreteGraphChartView.showsHorizontalReferenceLines = true
-        discreteGraphChartView.showsVerticalReferenceLines = true
-        
-        
-        
-        
-        // Bar Chart
-        
-//        guard let barGraphChartView = graphView as? ORK else { return }
-//        barGraphChartView.dataSource = barGraphChartDataSource
-//        barGraphChartView.tintColor = UIColor(red: 255/255, green: 152/255, blue: 0/255, alpha: 1)//Orange
-//        // Optional custom configuration
-//        barGraphChartView.showsHorizontalReferenceLines = true
-//        barGraphChartView.showsVerticalReferenceLines = true
     }
 
     /*
