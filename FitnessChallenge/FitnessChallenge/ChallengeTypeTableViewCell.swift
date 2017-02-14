26c5a3974a0a652d33c2c34f5fb06311e2529332
Aaron Martinez <aaronmartinez6@hotmail.com>
2017-02-13 18:02:58 -0700

    Change the way the challenges are viewed/created
    Also, add images for challenge type custom cells.

---
+++ FitnessChallenge/FitnessChallenge/ChallengeTypeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChallengeTypeTableViewCell.swift
+//  FitnessChallenge
+//
+//  Created by Aaron Martinez on 2/8/17.
+//  Copyright © 2017 Aaron Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeTypeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
